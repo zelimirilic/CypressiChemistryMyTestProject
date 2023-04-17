d83a718940bc0202792dcff94baf7da5a21c8402
zelimir <zelimir.ilic@consolia.rs>
2023-04-17 12:55:55 +0200

    CypressiChemistryMyTestProject

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 
 module.exports = defineConfig({
@@ -11,7 +12,7 @@ module.exports = defineConfig({
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
+      allureWriter(on, config);
       return config;
     },
   },
